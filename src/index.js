59760159462543ab0b5e4c4ab99956ffb9a9b9ea
Maxondria <maxtayebwa@gmail.com>
2020-05-15 16:03:45 +0300

    update broker

--- src/index.js
+++ src/index.js
@@ -1,104 +1,16 @@
 const cluster = require("cluster");
-const mqemitter = require("mqemitter-mongodb");
-const mongoPersistence = require("aedes-persistence-mongodb");
-
-const MONGO_URL = "mongodb://localhost:27017/aedes-clusters";
+const getAedes = require("./aedes");
 
 function startAedes() {
   const port = 1883;
 
-  const aedes = require("aedes")({
-    id: "BROKER_" + cluster.worker.id,
-    mq: mqemitter({
-      url: MONGO_URL,
-    }),
-    persistence: mongoPersistence({
-      url: MONGO_URL,
-      // Optional ttl settings
-      ttl: {
-        packets: 300, // Number of seconds
-        subscriptions: 300,
-      },
-    }),
-  });
-
-  aedes.authenticate = function (client, username, password, callback) {
-    const authorized =
-      username === "deviceId" && password.toString() === "apiKey";
-    client.user = JSON.stringify({ username, password: password.toString() });
-    callback(null, authorized);
-  };
+  const aedes = getAedes(cluster);
 
   const server = require("net").createServer(aedes.handle);
 
   server.listen(port, function () {
     console.log("Aedes listening on port:", port);
     aedes.publish({ topic: "aedes/hello", payload: "I'm broker " + aedes.id });
-  });
-
-  aedes.on("subscribe", function (subscriptions, client) {
-    console.log(
-      "MQTT client \x1b[32m" +
-        (client ? client.id : client) +
-        "\x1b[0m subscribed to topics: " +
-        subscriptions.map((s) => s.topic).join("\n"),
-      "from broker",
-      aedes.id
-    );
-  });
-
-  aedes.on("unsubscribe", function (subscriptions, client) {
-    console.log(
-      "MQTT client \x1b[32m" +
-        (client ? client.id : client) +
-        "\x1b[0m unsubscribed to topics: " +
-        subscriptions.join("\n"),
-      "from broker",
-      aedes.id
-    );
-  });
-
-  // fired when a client connects
-  aedes.on("client", function (client) {
-    console.log(
-      "Client Connected: \x1b[33m" + (client ? client.id : client) + "\x1b[0m",
-      "to broker",
-      aedes.id
-    );
-  });
-
-  // fired when a client disconnects
-  aedes.on("clientDisconnect", function (client) {
-    console.log(
-      "Client Disconnected: \x1b[31m" +
-        (client ? client.id : client) +
-        "\x1b[0m",
-      "to broker",
-      aedes.id
-    );
-  });
-
-  // fired when a message is published
-  aedes.on("publish", async function (packet, client) {
-    console.log(
-      "Client \x1b[31m" +
-        (client ? client.id : "BROKER_" + aedes.id) +
-        "\x1b[0m has published",
-      packet.payload.toString(),
-      "on",
-      packet.topic,
-      "to broker",
-      aedes.id
-    );
-    if (packet.topic === "device/can/log") {
-      const { topicOnclient, message } = JSON.parse(packet.payload.toString());
-
-      aedes.publish({
-        topic: topicOnclient,
-        payload:
-          "I was intercepted by broker but my original payload is:  " + message,
-      });
-    }
   });
 }
 
