b8e7e0e83f080e49d0507a01d4e68cc75a14e93b
Maxondria <maxtayebwa@gmail.com>
2020-05-15 16:02:59 +0300

    add test credentials and subscribe to events in pub

--- src/pub.js
+++ src/pub.js
@@ -1,19 +1,26 @@
 const mqtt = require("mqtt");
 
 const client = mqtt.connect("mqtt://localhost:1883", {
-  password: "apiKey",
-  username: "deviceId",
+  clientId: "5e8a07a2945ed570613752cf/S8MF0KT-36C4MTY-JDZC84X-8XN9C1D",
+  password: "S8MF0KT-36C4MTY-JDZC84X-8XN9C1D",
+  username: "5e8a07a2945ed570613752cf",
 });
 
+const LOGGED =
+  "5e8a07a2945ed570613752cf/S8MF0KT-36C4MTY-JDZC84X-8XN9C1D/LOGGED";
+const LOGGING_ERROR =
+  "5e8a07a2945ed570613752cf/S8MF0KT-36C4MTY-JDZC84X-8XN9C1D/ERROR";
+
 const topicToPublish = {
-  topic: "device/can/log",
-  messages: ["Message 1", "Message 2", "Message 3", "Message 4", "Message 5"],
+  topic: "device/data/LOG",
+  messages: ["20kwh", "30kwh", "40kwh", "50kwh", "60kwh"],
 };
 
 client.on("connect", () => {
+  client.subscribe(LOGGED);
+  client.subscribe(LOGGING_ERROR);
   // Demo Controller
   const { topic, messages } = topicToPublish;
-  const topicOnclient = "some-apikey/some-deviceid";
 
   const publishedMap = new Map();
 
@@ -22,13 +29,21 @@ client.on("connect", () => {
     const message = messages[random];
 
     if (!publishedMap.has(message)) {
-      client.publish(topic, JSON.stringify({ topicOnclient, message }));
+      client.publish(topic, JSON.stringify({ volts: random, watts: message }));
       publishedMap.set(message, true);
     } else if (publishedMap.size === messages.length) {
       clearInterval(Intv);
       console.log("All messages published");
     }
   }, 5000);
+});
+
+client.on("message", (topic, payload) => {
+  if (topic === LOGGED) {
+    console.log("Data Logged: ", payload.toString());
+  } else if (topic === LOGGING_ERROR) {
+    console.log("Error Logging: ", payload.toString());
+  }
 });
 
 //Handle errors
